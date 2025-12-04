c1ca0457075d91b3ca299f19228d3251cd4354ad
Brian Cortright <brcortright@gmail.com>
2025-12-04 11:59:01 -0800

    Address Next.js vulnerability and package dependency issues for lazy load

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    skipTrailingSlashRedirect: true,
-    images: { unoptimized: true },
-    compiler: { styledComponents: true },
-}
+  output: "export",
+  skipTrailingSlashRedirect: true,
+  images: { unoptimized: true },
+  compiler: { styledComponents: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/LazyWrapper/LazyWrapper.js
+++ src/components/LazyWrapper/LazyWrapper.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import LazyLoad from 'react-lazyload';
+import React, { useState, useEffect, ReactNode, useRef } from "react";
+import styled from "styled-components";
 
-const LazyWrapper = ({
-  children,
-  lazyLoad = true,
-  ...props
-}) => {
+const LazyDiv = styled.div`
+  ${({ $height, $isInView }) =>
+    $height && !$isInView && `min-height: ${$height}px;`}
+`;
+
+const LazyWrapper = ({ children, enable = true, height, offset }) => {
+  const [isInView, setIsInView] = useState(!enable);
+  const wrapperRef = useRef(null);
+
+  const observerCallback = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting || entry.boundingClientRect.top <= 0 + offset) {
+      setIsInView(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!enable) return;
+
+    const wrapper = wrapperRef.current;
+
+    const observer = new IntersectionObserver(observerCallback, {
+      rootMargin: `${offset}px`,
+      threshold: 0,
+    });
+
+    if (wrapper) {
+      observer.observe(wrapper);
+    }
+
+    return () => {
+      if (wrapper) {
+        observer.unobserve(wrapper);
+      }
+    };
+  }, [enable, offset]);
+
+  const styleOnbject = {};
+
+  if (height) {
+    styleOnbject["height"] = `${height}px`;
+  }
+
   return (
-    <>
-      {lazyLoad ? (
-        <LazyLoad {...props}>{children}</LazyLoad>
-      ) : (
-        <>{children}</>
-      )}
-    </>
+    <LazyDiv ref={wrapperRef} $height={height} $isInView={isInView}>
+      {isInView ? children : null}
+    </LazyDiv>
   );
 };
 
