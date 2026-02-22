e787fbd7f90c8db171d9f3de49572fb14e33ea57
Brian Cortright <brcortright@gmail.com>
2026-02-21 21:58:23 -0800

    fugg these bots

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import Script from 'next/script'
 import './globals.css'
 import StyledComponentsRegistry from './registry'
 
@@ -10,14 +9,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body>
-        <Script
+      <head>
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6675084090356256"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
+      </head>
+      <body>
         <StyledComponentsRegistry>
           {children}
         </StyledComponentsRegistry>
