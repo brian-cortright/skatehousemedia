a748125af9f180542a68cb8130d190dc8afc8867
Brian Cortright <brcortright@gmail.com>
2026-02-22 19:33:20 -0800

    Archive page ad

--- src/app/archive/page.js
+++ src/app/archive/page.js
@@ -8,6 +8,7 @@ import VideoCard from "#/components/VideoCard";
 import LazyWrapper from "#/components/LazyWrapper/LazyWrapper";
 import { basePadding } from "#/theme";
 import SearchBar from "#/components/SearchBar/SearchBar";
+import Script from "next/script";
 
 const Archive = () => {
   const [searchInput, setSearchInput] = useState("");
@@ -30,6 +31,23 @@ const Archive = () => {
     <>
       <BackButtonBar target={"/"} />
       <PageWrapper>
+        <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6675084090356256"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-6675084090356256"
+          data-ad-slot="4725789316"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+        <Script id="adsense-init" strategy="afterInteractive">
+          {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+        </Script>
         <Headline
           as="h1"
           margin={`0 auto ${basePadding.xLarge} auto`}
