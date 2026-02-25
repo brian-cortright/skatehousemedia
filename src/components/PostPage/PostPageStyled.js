bf59d795e92c8c437a52d411ee6db9266b7f9d52
Brian Cortright <brcortright@gmail.com>
2026-02-25 13:38:42 -0800

    More post styling

--- src/components/PostPage/PostPageStyled.js
+++ src/components/PostPage/PostPageStyled.js
@@ -24,8 +24,49 @@ export const ContentWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   margin-block-start: ${basePadding.large};
+  margin-block-end: ${basePadding.xxxxLarge};
   margin-inline: auto;
   max-width: 900px;
   padding: 0 ${basePadding.large};
   width: 100%;
+`;
+
+export const TaxonomyWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin-block: ${basePadding.large};
+  padding: 0 ${basePadding.large};
+`;
+
+export const CategoriesWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  margin-block: ${basePadding.base};
+  padding: 0 ${basePadding.large};
+
+  a {
+    padding-inline-end: 8px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const TagsWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  margin-block: ${basePadding.base};
+  padding: 0 ${basePadding.large};
+
+  a {
+    padding-inline-end: 8px;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
